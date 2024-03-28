fd06a38b7664c4f435101e0c9731cf45eaa602ca
felix-zhaolei <felix@portkey.finance>
2024-03-28 17:54:06 +0800

    modify: using connection.

--- src/IM.Application/Dapper/Repository/ImRepository.cs
+++ src/IM.Application/Dapper/Repository/ImRepository.cs
@@ -15,26 +15,17 @@ public class ImRepository<T> : IImRepository<T> where T : class, ISingletonDepen
         _dapperContext = dapperContext;
     }
 
-    public T QueryFirstOrDefault(string sql, object param = null)
-    {
-        return _dapperContext.OpenConnection().QueryFirstOrDefault<T>(sql, param);
-    }
-
     public Task<T> QueryFirstOrDefaultAsync(string sql, object param = null)
     {
-        return _dapperContext.OpenConnection().QueryFirstOrDefaultAsync<T>(sql, param);
-    }
-
-    public IEnumerable<T> Query(string sql, object param = null, IDbTransaction transaction = null,
-        bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
-    {
-        return _dapperContext.OpenConnection().Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
+        using var connection = _dapperContext.OpenConnection();
+        return connection.QueryFirstOrDefaultAsync<T>(sql, param);
     }
 
     public Task<IEnumerable<T>> QueryAsync(string sql, object param = null, IDbTransaction transaction = null,
         int? commandTimeout = null, CommandType? commandType = null)
     {
-        return _dapperContext.OpenConnection().QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
+        using var connection = _dapperContext.OpenConnection();
+        return connection.QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
     }
 }
 
@@ -47,26 +38,17 @@ public class ImRepository : IImRepository, ISingletonDependency
         _dapperContext = dapperContext;
     }
 
-    public T QueryFirstOrDefault<T>(string sql, object param = null)
-    {
-        return _dapperContext.OpenConnection().QueryFirstOrDefault<T>(sql, param);
-    }
-
     public Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null)
     {
-        return _dapperContext.OpenConnection().QueryFirstOrDefaultAsync<T>(sql, param);
+        using var connection = _dapperContext.OpenConnection();
+        return connection.QueryFirstOrDefaultAsync<T>(sql, param);
     }
-
-    public IEnumerable<T> Query<T>(string sql, object param = null, IDbTransaction transaction = null,
-        bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
-    {
-        return _dapperContext.OpenConnection().Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
-    }
-
+    
     public Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null,
         int? commandTimeout = null, CommandType? commandType = null)
     {
-        return _dapperContext.OpenConnection().QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
+        using var connection = _dapperContext.OpenConnection();
+        return connection.QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
     }
 
     public async Task<(IEnumerable<T> data, int totalCount)> QueryPageAsync<T>(string sql, object param = null,
