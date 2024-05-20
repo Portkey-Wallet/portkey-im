07ae7433e9207cd853885744d11e828bf194a939
Edward-BXS <Magic.Wizard@portkey.finance>
2024-05-20 13:29:02 +0800

    fix

--- src/IM.Application/ImApplicationAutoMapperProfile.cs
+++ src/IM.Application/ImApplicationAutoMapperProfile.cs
@@ -52,7 +52,7 @@ public class ImApplicationAutoMapperProfile : Profile
         CreateMap<UserIndex, ImUser>()
             .ForMember(t => t.PortkeyId, m => m.MapFrom(f => f.Id));
         CreateMap<ReportUserImMessageCmd, ReportedMessage>()
-            .ForMember(t => t.ReportTime, m => m.MapFrom(f => DateTime.UtcNow.Ticks));
+            .ForMember(t => t.ReportTime, m => m.MapFrom(f => (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000));
         CreateMap<CaHolderInfoDto, CaHolderDto>()
             .ForMember(t => t.UserId, m => m.MapFrom(f => f.UserId == Guid.Empty ? null : f.UserId.ToString()))
             ;
